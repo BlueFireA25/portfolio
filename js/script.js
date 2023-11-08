6fba5e23c001f9231d152eb9322e173964feef9a
Sebastián Castro <sebastian@snabb.cl>
2023-11-08 17:56:10 -0300

    tABLE STYLES

--- js/script.js
+++ js/script.js
@@ -1,13 +1,41 @@
 function createLink(text, href) {
     const link = document.createElement("a");
+    const icon = document.createElement("i");
+
     link.href = href;
     link.textContent = text;
+    icon.className = "uil uil-external-link-alt icon";
+    link.appendChild(icon);
+
     return link;
+
+}
+
+function createLinkTitle(text, href) {
+    if (window.innerWidth < 768) {
+        const link = document.createElement("a");
+        const icon = document.createElement("i");
+
+        link.href = href;
+        link.textContent = text;
+        link.className = 'table__tbody--project'
+        icon.className = "uil uil-external-link-alt icon";
+        link.appendChild(icon);
+
+        return link;
+    } else {
+        const link = document.createElement("span");
+
+        link.textContent = text;
+        link.className = 'table__tbody--project'
+
+        return link;
+    }
 }
 
 const data = [
-    ["Halloween Store", "HTML, CSS, JavaScript", createLink('hola', 'index.html')],
-    ["Halloween Store 2", "HTML, CSS, JavaScript", createLink('chao', 'otro.html')],
+    [createLinkTitle('Halloween Store', 'otro.html'), "HTML, CSS, JavaScript", createLink('hola', 'index.html')],
+    [createLinkTitle('Christmas Store', 'otro.html'), "HTML, CSS, JavaScript", createLink('chao', 'otro.html')],
 ];
 
 new gridjs.Grid({
@@ -17,11 +45,20 @@ new gridjs.Grid({
         },
         {
             name: 'Built with',
+            hidden: window.innerWidth < 425
+
         },
         {
             name: 'Link',
-
+            hidden: window.innerWidth < 768
         },
     ],
     data: data,
+    className: {
+        table: 'table',
+        container: 'table__container',
+        tbody: 'table__tbody',
+        td: 'table__tbody--td',
+        th: 'table__tbody--th',
+    },
 }).render(document.getElementById("wrapper"));
